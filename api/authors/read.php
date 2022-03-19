c214187fb82cbd23b158b89012e7b0133e0a2bee
Jonah Wagoner <wagoner.jonah@gmail.com>
2022-03-19 19:58:47 -0400

    get all returns list directly

--- api/authors/read.php
+++ api/authors/read.php
@@ -10,7 +10,6 @@
   if($num > 0) {
         // Cat array
         $cat_arr = array();
-        $cat_arr['data'] = array();
 
         while($row = $result->fetch(PDO::FETCH_ASSOC)) {
           extract($row);
@@ -21,7 +20,7 @@
           );
 
           // Push to "data"
-          array_push($cat_arr['data'], $cat_item);
+          array_push($cat_arr, $cat_item);
         }
 
         // Turn to JSON & output
